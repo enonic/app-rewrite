b14e449b0d31d5f60d5f2a314af8729d9f144759
Runar Andre Myklebust <runar@rett24.no>
2020-09-04 13:25:33 +0200

    refactored rendering request-tester

--- src/main/resources/assets/src/js/toolTester.js
+++ src/main/resources/assets/src/js/toolTester.js
@@ -49,17 +49,19 @@ let testRequest = function () {
         type: 'GET',
         data: data,
         error: function (response, status, error) {
-            $(model.elements.redirectSteps).html(response.responseText);
+            renderError(response);
+
         },
         success: function (result) {
-            processResult(result);
+            $(model.elements.requestTesterResult).html(result);
         }
     });
 };
 
-let processResult = function (result) {
-    writeRequestRoute(result);
-};
+
+function renderError(result) {
+
+}
 
 function renderHeading(result) {
     const headingText = buildHeadingText(result);
@@ -81,94 +83,6 @@ let buildHeadingText = function (result) {
     } else {
         return result.state;
     }
-};
-
-let writeRequestRoute = function (result) {
-    let html = renderHeading(result);
-    html += renderSteps(result);
-    html += "</div>";
-    $(model.elements.requestTesterResult).html(html);
-};
-
-function renderSteps(result) {
-    let html = '<div id="request-tester-steps">';
-
-    const steps = result.steps;
-
-    for (let i = 0; i < steps.length; i++) {
-
-        html += renderStep(steps[i]);
-        if (steps[i + 1]) {
-            html += renderStepTransition();
-        }
-    }
-    return html;
-}
-
-let renderStep = function (step) {
-
-    if (!step || !step.rewrite) {
-        return "";
-    }
-
-    const incomingRequest = step.incomingRequest;
-    const matchingVHost = incomingRequest.matchingVHost;
-
-    let html = "<div class='redirect-step'>";
-    html += renderIncomingRequest(matchingVHost, incomingRequest);
-    html += renderRewriteTarget(step);
-    html += "</div>";
-
-    return html;
-};
-
-function renderIncomingRequest(matchingVHost, incomingRequest) {
-    let html = "<div class='step-result-box incoming-request'>";
-    html += "<div class='vhost'><p class='label'>vhost:</p><p class='value'>"
-    if (matchingVHost) {
-        html += matchingVHost.name;
-    } else {
-        html += "No match";
-    }
-    html += "</p></div>";
-    html += "<div class='url'><p class='label'>request:</p><p class='value'>" + incomingRequest.url + "</p></div>";
-    html += "</div>";
-    return html;
-}
-
-function renderRewriteTarget(step) {
-
-    const rewriteTarget = step.rewrite.target;
-    const rewriteCode = step.rewrite.code;
-    let html = "";
-
-    if (rewriteTarget) {
-        html += "<div class='transition'><i class=\"material-icons\">arrow_forward</i><p>(" + rewriteCode + ")</p></div>";
-    } else {
-        html += "<div class='transition'></div>";
-    }
-
-    if (rewriteTarget) {
-        html += "<div class='step-result-box rewrite-target'>";
-        html += "<div class='type type-" + rewriteTarget.type + "'>";
-        html += "<p class='label'>type:</p><p class='value'>" + rewriteTarget.type + "</p>";
-        html += "</div>";
-        html += "<div class='target'>";
-        html += "<p class='label'>target:</p><p class='value'>" + rewriteTarget.url + "</p>";
-        html += "</div>";
-        html += "</div>";
-    } else {
-        html += "<div class='rewrite-target'></div>";
-    }
-
-    return html;
-}
-
-let renderStepTransition = function () {
-    let html = "<div class='stepTransition'>";
-    html += "<i class=\"material-icons\">arrow_drop_down</i>";
-    html += "</div>";
-    return html;
 };
 
 let delay = (function () {
--- src/main/resources/services/request-tester/request-tester.js
+++ src/main/resources/services/request-tester/request-tester.js
@@ -1,6 +1,8 @@
 let rewriteDao = require('/lib/rewrite-dao');
+let thymeleaf = require('/lib/thymeleaf');
 
 exports.get = function (req) {
+
 
     let requestURL = req.params.requestURL;
 
@@ -20,17 +22,99 @@ exports.get = function (req) {
 
         log.error("Error in request tester: %s", JSON.stringify(result.error, null, 2));
 
+        let view = resolve('request-tester-error.html');
+
+        let model = {
+            heading: {
+                text: "Result: error",
+                class: "error"
+            },
+            message: result.error
+        };
+
         return {
-            status: 500,
-            body: result.error,
-            contentType: 'application/json'
+            contentType: 'text/html',
+            body: thymeleaf.render(view, model)
         }
     }
 
+    const model = createModel(result);
+    let view = resolve('request-tester-result.html');
     return {
-        status: 200,
-        body: result,
-        contentType: 'application/json'
+        contentType: 'text/html',
+        body: thymeleaf.render(view, model)
+    };
+};
+
+let createModel = function (result) {
+    let model = {
+        heading: {
+            text: "Result: " + createHeadingText(result),
+            class: result.state
+        },
+        steps: createStepsModel(result)
+    };
+    return model;
+};
+
+let createHeadingText = function (result) {
+
+    if (result.state === "OK") {
+        let redirects = 0;
+        result.steps.forEach(function (step) {
+            if (step.rewrite.target) {
+                redirects++;
+            }
+        });
+        return redirects + (redirects === 1 ? " redirect " : " redirects");
+
+    } else {
+        return result.state;
+    }
+};
+
+let createStepsModel = function (result) {
+
+    let steps = [];
+
+    for (let i = 0; i < result.steps.length; i++) {
+        steps.push(createStepModel(result.steps[i]));
     }
 
+    return steps;
+};
+
+let createStepModel = function (step) {
+    const incomingRequest = step.incomingRequest;
+    const matchingVHost = incomingRequest.matchingVHost;
+    const rewriteTarget = step.rewrite.target;
+    const rewriteCode = step.rewrite.code;
+
+    const stepModel = {
+        request: {
+            url: incomingRequest.url,
+            vhost: matchingVHost ? matchingVHost.name : "no match"
+        }
+    };
+
+    const stepIsRewrite = step.rewrite && step.rewrite.target;
+
+    if (stepIsRewrite) {
+
+        stepModel.target = {
+            class: rewriteTarget.type,
+            url: rewriteTarget.url,
+            type: rewriteTarget.type
+        };
+
+        stepModel.rewrite = {
+            code: rewriteCode
+        };
+    }
+
+    stepModel.nextTransition = {
+        hasNext: stepIsRewrite
+    };
+
+    return stepModel
 };
