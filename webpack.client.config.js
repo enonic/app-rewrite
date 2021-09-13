db85fb67f3c79012542b761f24c13ab28c62e9a2
Alan Semenov <ase@enonic.com>
2021-09-13 14:12:39 +0200

    Fixed terser config

--- webpack.client.config.js
+++ webpack.client.config.js
@@ -32,7 +32,6 @@ const config = {
     optimization: {
         minimizer: [
             new TerserPlugin({
-                sourceMap: true,
                 terserOptions: {
                     compress: {
                         drop_console: false,
